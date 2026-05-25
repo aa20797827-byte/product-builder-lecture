47ffec6f52798fb57bfabe7e177378a6d0b9e7f7
aa20797827 <aa20797827@gmail.com>
2026-05-25 19:14:14 +0000

    feat: 반려동물 정보 아티클 4개 추가 + canonical URL 수정 + 사이트맵 업데이트 (애드센스/SEO)

--- api/sitemap.js
+++ api/sitemap.js
@@ -33,6 +33,30 @@ module.exports = async function handler(req, res) {
     <changefreq>monthly</changefreq>
     <priority>0.4</priority>
   </url>
+  <url>
+    <loc>https://pawpickai.vercel.app/%EA%B0%95%EC%95%84%EC%A7%80-%EB%B0%A5-%EC%95%88%EB%A8%B9%EC%96%B4%EC%9A%94.html</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://pawpickai.vercel.app/%EA%B0%95%EC%95%84%EC%A7%80-%EC%98%81%EC%96%91%EC%A0%9C-%EC%B6%94%EC%B2%9C.html</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://pawpickai.vercel.app/%EA%B3%A0%EC%96%91%EC%9D%B4-%EA%B5%AC%ED%86%A0-%EC%9B%90%EC%9D%B8.html</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://pawpickai.vercel.app/%EB%B0%98%EB%A0%A4%EB%8F%99%EB%AC%BC-%EC%82%AC%EB%A3%8C-%EC%84%A0%ED%83%9D%EB%B2%95.html</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
 </urlset>`;
 
   res.status(200).send(sitemap);
